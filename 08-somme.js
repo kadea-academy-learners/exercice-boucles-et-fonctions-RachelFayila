859a87852c5e4fe00163a1c06a7f655d90e7447d
Rachel fayila <fayilarachel72@gmail.com>
2025-10-23 19:47:08 +0100

    feat: add exercice somme.js

--- 08-somme.js
+++ 08-somme.js
@@ -10,10 +10,18 @@
   Placeholder : levée d'erreur pour indiquer que l'exercice doit être implémenté.
 */
 function somme(n) {
-  // Exercice non implémenté : retourner la somme de 1 à n
-  // Placeholder neutre : retourne 0 pour indiquer non-implémentation
-  return 0;
+  if (typeof n !== "number" || n < 0)
+  {
+   return 0; 
+  }
+  let somme = 0;
+  for (let i = 1; i <= n ; i++)
+  {
+    somme += i;
+  }
+  return somme;
 }
+console.log(somme (8));
 
 // Ne pas modifier la ligne ci-dessous
 module.exports = { somme }
